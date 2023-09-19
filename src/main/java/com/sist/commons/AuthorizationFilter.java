dacf07d87f35dca3e140c405c3c1f92832acfb5e
YeonsangYoon <1004dustkd@gmail.com>
2023-09-19 20:11:20 +0900

    인증/인가 처리 Filter로 교체

--- src/main/java/com/sist/commons/AuthorizationFilter.java
+++ src/main/java/com/sist/commons/AuthorizationFilter.java
@@ -1,17 +1,25 @@
 package com.sist.commons;
 
 import org.springframework.util.PatternMatchUtils;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
+public class AuthorizationFilter implements Filter {
     // white list : 모든 사용자 접근 가능 url
     private static final String[] whiteList = {
             // 메인 화면
             "/mingle/main/main.do",
+            // 정적 리소스
+            "/mingle/css/*",
+            "/mingle/fonts/*",
+            "/mingle/images/*",
+            "/mingle/img/*",
+            "/mingle/js/*",
+            "/mingle/sass/*",
             // 인증 관련 요청
             "/mingle/auth/*",
             // 공지사항 관련 요청
@@ -35,26 +43,41 @@ public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
             "/mingle/mento/follow.do"
     };
 
+    private boolean isWhiteList(String uri){
+        return PatternMatchUtils.simpleMatch(whiteList, uri);
+    }
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // request, response, session 객체
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpSession session = request.getSession();
+
+        // UserId, 요청 uri 추출
         String id = (String)session.getAttribute("id");
         String uri = request.getRequestURI();
 
         if(!isWhiteList(uri)){ // 요청 url이 whitelist가 아닌 경우
             if(id == null){ // 로그인 안됐을 경우 redirect
                 response.sendRedirect("/mingle/auth/login_require.do");
-                return false;
+                return;
             }
             else if(PatternMatchUtils.simpleMatch("/mingle/admin/*", uri)
                     && !id.equals("admin")){ // 관리자가 아닌 유저가 관리자 페이지에 접근한 경우
                 response.sendRedirect("/mingle/auth/admin_require.do");
             }
         }
-        return true;
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    private boolean isWhiteList(String uri){
-        return PatternMatchUtils.simpleMatch(whiteList, uri);
+    @Override
+    public void destroy() {
+
     }
 }
